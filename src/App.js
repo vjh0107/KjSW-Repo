87137511f90566da15f9012b2e0ed8ca08f7f015
ryudonghwa <jaedori1@naver.com>
2021-01-30 14:21:53 +0900

    e

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="biggest">
+            <div className="main-title"><strong>FBI waring : if you were gay, call this phonenumber</strong></div>
+            <div className="naeyong">류동화는 2021년 1월 30일 (토요일) 오전 9시 30분경 실종되었습니다<br/>we found out park jun hyeong!</div>
+            <div className="missing">If Anyone Findthem Plz call 010-6409-71XX</div>
+        </div>
+    );
+}
+export default App;
